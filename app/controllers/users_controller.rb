41c2cb2cbe793044178bff7ba2ea5836dd4f95a1
lamchristine <christine.lam.06@gmail.com>
2016-05-12 12:24:46 -0700

    added new and create to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.avatar == ""
+      #Adds default avatar image if no image is uploaded
+      @user.avatar = "default_avatar.jpeg"
+    end
+    if @user.save
+      login(@user)
+      redirect_to root_path
+    else
+      flash[:error] = @user.errors.full_messages.to_sentenaces
+      redirect_to new_user_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :avatar)
+  end
+
 end
