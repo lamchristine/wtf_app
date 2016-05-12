64469aa49cefcd9410ae40c37c3b8f189e6f0009
eamoses <e.a.moses@gmail.com>
2016-05-12 15:08:01 -0700

    added devise

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,24 +1,2 @@
 module SessionsHelper
-
-  def login(user)
-    session[:user_id] = user.id
-    @current_user = user
-  end
-
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
-  end
-
-  def logged_in?
-    if current_user == nil
-      flash[:error] = "You must be logged in to perform this action"
-      redirect_to new_user_session_path
-    end
-  end
-
-  def logout
-    @current_user = session[:user_id] = nil
-  end
-
-
 end
