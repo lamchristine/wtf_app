32801119bcd6a5aa6c12f668f646b9f6c6184c36
lamchristine <christine.lam.06@gmail.com>
2016-05-12 23:28:29 -0700

    delete and edit user profile working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-    render :new
-  end
-
   def show
       @user = User.find_by(id: params[:id])
       if current_user == @user
@@ -13,59 +8,6 @@ class UsersController < ApplicationController
         flash[:error] = "This profile is private."
         redirect_to root_path
       end
-  end
-
-  def create
-    @user = User.create(user_params)
-    if @user.avatar == ""
-      #Adds default avatar image if no image is uploaded
-      @user.avatar = "default_avatar.jpeg"
-    end
-    if @user.save
-      login(@user)
-      redirect_to root_path
-    else
-      flash[:error] = @user.errors.full_messages.to_sentences
-      redirect_to new_user_path
-    end
-  end
-
-  def edit
-    @user = User.find_by(id: params[:id])
-    if current_user == @user
-      redirect_to edit_user_registration_path
-    else
-      flash[:error] = "You cannot edit another users profile"
-      redirect_to user_path
-    end
-  end
-
-  def update
-    debugger
-    @user = User.find_by(id: params[:id])
-    # if @user == current_user
-    #   @user.update(user_params)
-    #   if @user.avatar == ""
-    #     #Adds default avatar image if no image is uploaded
-    #     @user.avatar = "userprofile.png"
-    #     @user.save
-    #   end
-    #   redirect_to user_path
-    # else
-    #   flash[:error] = "You cannot edit another users profile"
-    #   redirect_to user_path
-    # end
-  end
-
-  def destroy
-    user = User.find_by(id: params[:id])
-    user.destroy
-    # if allowed?(user.id) && user.destroy
-    #   flash[:notice] = "Sorry to see you go. Account successfully deleted"
-    #   redirect_to index_path
-    # else
-    #   redirect_to user_path
-    # end
   end
 
   private
