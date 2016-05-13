dad0ea5e410c1060334ff35b08245328b6a1d023
eamoses <e.a.moses@gmail.com>
2016-05-13 15:10:00 -0700

    deleting changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname, :avatar, :password])
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password, :current_password) }
   end
+
 end
