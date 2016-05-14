12bd2b9f0c030ef7abb89e47e5cb20baa69d2383
eamoses <e.a.moses@gmail.com>
2016-05-13 21:48:33 -0700

    i dont know what im doing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,8 +27,6 @@ function setMapHeight(){
   $("#map").css("height", $( window ).height() - navHeight);
 }
 
-
-
 $(document).ready(function(){
   // setMapHeight();
   // googleMapBuild();
@@ -63,8 +61,6 @@ function googleMapBuild() {
     },
     function(){
       markers = handler.addMarkers([
-
-
         {
           "lat": latitude,
           "lng": longitude,
@@ -136,10 +132,7 @@ function googleMapBuild() {
       handler.fitMapToBounds();
       handler.getMap().setZoom(10);
     });
-
-
 }
-
 
 function geoFindMe() {
   var output = document.getElementById("out");
@@ -152,9 +145,9 @@ function geoFindMe() {
   function success(position) {
     latitude  = position.coords.latitude;
     longitude = position.coords.longitude;
-    console.log('Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°');
+    console.log('Latitude is ' + latitude + '°Longitude is ' + longitude + '°');
     var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300";
 
     console.log($('body').attr('class'));
 
@@ -169,69 +162,4 @@ function geoFindMe() {
 
   console.log("Locating…");
   navigator.geolocation.getCurrentPosition(success, error);
-}
-
-
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: "latitude", lng: "longitude"},
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length === 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-
 }
--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,117 +1,62 @@
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
 
-//
-// var navHeight = 52;
-// var latitude;
-// var longitude;
-// function setMapHeight(){
-//   $("#map").css("height", $( window ).height() - navHeight);
-//   console.log("setMapHeight");
-// }
-//
-//
-// $(document).ready(function(){
-//   // setMapHeight();
-//   // googleMapBuild();
-//   // geoFindMe();
-//   setMapHeight();
-//   geoFindMe();
-//   // console.log(window.location.search);
-//
-//   $('.event-btn').click(function(){
-//     console.log("hi");
-//
-//     // push something to the btn css class via jquery
-//
-//     // function setParams() {
-//     //   var queryString = "&latitude="+latitude+"&longitude="+longitude;
-//     //   window.location.search = queryString;
-//     //
-//     // }
-//     // setParams();
-//     // return;
-//
-//
-//   });
-//
-// });
-//
-// $(document).on("page:load", function(){
-//   // sets Google Map height on load
-//   setMapHeight();
-//   geoFindMe();
-//
-//   // googleMapBuild();
-// });
-//
-// // resets Google Map height on change of screen size
-// $(window).resize(function(){
-//   setMapHeight();
-//   // geoFindMe();
-// });
-//
-// function googleMapBuild() {
-//   console.log("googleMapBuild");
-//   handler = Gmaps.build('Google');
-//   handler.buildMap({
-//       provider: {
-//         disableDefaultUI: true
-//         // pass in other Google Maps API options here
-//       },
-//       internal: {
-//         id: 'map'
-//       }
-//     },
-//     function(){
-//       markers = handler.addMarkers([
-//         {
-//           "lat": latitude,
-//           "lng": longitude,
-//           "picture": {
-//             "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
-//             "width":  32,
-//             "height": 32
-//           },
-//           "infowindow": "hello!"
-//         }
-//       ]);
-//       handler.bounds.extendWith(markers);
-//       handler.fitMapToBounds();
-//       handler.getMap().setZoom(19);
-//     });
-//
-// }
-//
-//
-//
-// function geoFindMe() {
-//   console.log("geoFindMe");
-//   var output = document.getElementById("out");
-//
-//   if (!navigator.geolocation){
-//     console.log( "Geolocation is not supported by your browser");
-//     return;
-//   }
-//
-//   function success(position) {
-//     latitude  = position.coords.latitude;
-//     longitude = position.coords.longitude;
-//     console.log('Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°');
-//     var img = new Image();
-//     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-//
-//     console.log("geoFindMe:success");
-//     // console.log(queryString);
-//
-//     googleMapBuild();
-//
-//   }
-//
-//   function error() {
-//     console.log("Unable to retrieve your location");
-//   }
-//
-//   console.log("Locating…");
-//   navigator.geolocation.getCurrentPosition(success, error);
-// }
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length === 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
