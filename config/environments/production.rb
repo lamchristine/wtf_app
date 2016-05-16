2a8b9a9c9a625221082f9e42ff0c9243e44c5e85
hectorcoronado <coronado.hector@gmail.com>
2016-05-16 10:45:01 -0700

    commit before pulling origin develoment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,13 +84,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
