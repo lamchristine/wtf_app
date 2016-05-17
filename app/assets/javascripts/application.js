3bcdea20d247d1445ebba969457e99ec9d568157
lamchristine <christine.lam.06@gmail.com>
2016-05-17 13:54:56 -0700

    works but only on refresh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,47 +18,46 @@
 //= require gmaps/google
 //= require_tree .
 
-var navHeight = 52;
-
-function setMapHeight(){
-  $("#map").css("height", $( window ).height() - navHeight);
-}
-
-$(document).ready(function(){
-  setMapHeight();
-  // googleMapBuild();
-  geoFindMe();
-
-  $('.like').click( function() {
-    console.log("liked!");
-  });
-});
-
-$(document).on("page:load", function(){
-  setMapHeight();
-  geoFindMe();
-});
-
-// resets Google Map height on change of screen size
-$(window).resize(function(){
-  setMapHeight();
-  // geoFindMe();
-});
-
-
-function geoFindMe() {
-
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    console.log(latitude);
-    console.log(longitude);
-    $('#coordinates').val(latitude + ',' + longitude);
-  }
-
-  function error() {
-    alert("Unable to retrieve your location");
-  }
-
-  navigator.geolocation.getCurrentPosition(success, error);
-}
+// var navHeight = 52;
+//
+// function setMapHeight(){
+//   $("#map").css("height", $( window ).height() - navHeight);
+// }
+//
+// $(document).ready(function(){
+//   setMapHeight();
+//   // googleMapBuild();
+//   geoFindMe();
+//
+//   $('.like').click( function() {
+//     console.log("liked!");
+//   });
+// });
+//
+// $(document).on("page:load", function(){
+//   setMapHeight();
+//   geoFindMe();
+// });
+//
+// // resets Google Map height on change of screen size
+// $(window).resize(function(){
+//   setMapHeight();
+//   // geoFindMe();
+// });
+//
+//
+// function geoFindMe() {
+//
+//   function success(position) {
+//     var latitude  = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//     $('#coordinates').val(latitude + ',' + longitude);
+//     console.log(latitude);
+//   }
+//
+//   function error() {
+//     alert("Unable to retrieve your location");
+//   }
+//
+//   navigator.geolocation.getCurrentPosition(success, error);
+// }
--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,1 +1,47 @@
 
+var navHeight = 52;
+
+function setMapHeight(){
+  $("#map").css("height", $( window ).height() - navHeight);
+}
+
+$(document).ready(function(){
+  setMapHeight();
+  // googleMapBuild();
+  geoFindMe();
+
+  $('.like').click( function() {
+    console.log("liked!");
+  });
+});
+
+$(document).on("page:load", function(){
+  setMapHeight();
+  geoFindMe();
+});
+
+// resets Google Map height on change of screen size
+$(window).resize(function(){
+  setMapHeight();
+  // geoFindMe();
+});
+
+
+function geoFindMe() {
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    $('#coordinates').val(latitude + ',' + longitude);
+    var coordinates = latitude + ',' + longitude;
+    $('#map').data("latitude", latitude);
+    $('#map').data("longitude", longitude);
+    alert(coordinates);
+  }
+
+  function error() {
+    alert("Unable to retrieve your location");
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
