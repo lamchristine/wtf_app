3b051e6b3872fd95b1e8cda3dbe8993e139f1536
eamoses <e.a.moses@gmail.com>
2016-05-18 11:37:22 -0700

    changed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.destroy_all
 Event.destroy_all
+User.destroy_all
 
 thomas = User.create(
   avatar: "thomas-profile.jpeg",
