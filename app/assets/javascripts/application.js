db0f8920b5c29fd4ec179d4b467c6bdbcdd8a3c9
lamchristine <christine.lam.06@gmail.com>
2016-06-14 17:14:59 -0400

    removed config.assets.initialize_on_precompile and add in bootstrap sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-3.3.6.min
+//= require bootstrap-sprockets
 //= require underscore
 //= require gmaps/google
 //= require_tree .
